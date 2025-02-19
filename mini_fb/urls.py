0054644a4cd68fa7347f468208308de5711ae32c
Milo Zurn-Galinsky <mish@Milos-MacBook-Pro.local>
2025-02-19 15:23:38 -0500

    finished the show all page minus css styling

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -1,8 +1,8 @@
 # mini_fb/urls.py
 
 from django.urls import path
-from django.conf import settings
+from .views import ShowAllProfilesView
 
 urlpatterns = [ 
-    
+    path('', ShowAllProfilesView.as_view(), name = "show_all_profiles"),
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,3 +1,13 @@
+# mini_fb/views.py
+
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Profile
 
 # Create your views here.
+class ShowAllProfilesView(ListView):
+    '''Define a view class to show all Profiles'''
+
+    model = Profile
+    template_name = 'mini_fb/show_all_profiles.html'
+    context_object_name = 'profiles'
